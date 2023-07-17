93409348d3402e101080bd86fd3bf9a55d481e71
Rora Alem <rora.goitom@gmail.com>
2023-07-17 18:24:37 -0400

    Implement renderTweets function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,13 +3,54 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+
 $(document).ready(function() {
-  function createTweetElement(tweet) {
+  // Fake data taken from initial-tweets.json
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd"
+      },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ];
+
+  //function for taking in an array of tweet objects
+  const renderTweets = function(tweets) {
+    //loop through tweets
+    for (const tweet of tweets) {
+      //call createTweetElement for each tweet
+      const $tweet = createTweetElement(tweet);
+      // take return value and appends it to the tweets container
+      $('#tweets-container').append($tweet);
+    }
+  };
+
+  //function that will generate the DOM structure for given tweet object
+  const createTweetElement = function(tweet) {
+    const createdAt = moment(tweet.created_at).fromNow();
     const $tweet = $(`
       <article class="tweet">
         <header>
           <div class="user-info">
-            <img class="avatar" src="${tweet.user.avatars}" alt="User Avatar">
+            <img src="${tweet.user.avatars}" alt="User Avatar">
             <h3 class="username">${tweet.user.name}</h3>
           </div>
           <span class="handle">${tweet.user.handle}</span>
@@ -18,39 +59,21 @@ $(document).ready(function() {
           <p>${tweet.content.text}</p>
         </div>
         <footer>
-          <span class="timestamp">${tweet.created_at}</span>
-          <div class="actions">
-            <i class="fas fa-flag"></i>
-            <i class="fas fa-retweet"></i>
-            <i class="fas fa-heart"></i>
+          <span class="timestamp">${createdAt}</span>
+          <div class="icons">
+          <i class="fa-solid fa-flag"></i>
+          <i class="fa-solid fa-retweet"></i>
+          <i class="fa-solid fa-heart"></i>
           </div>
         </footer>
       </article>
     `);
-  
+
     return $tweet;
-  }
-  // Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-  "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-  "created_at": 1461116232227
-}
+  };
 
-const $tweet = createTweetElement(tweetData);
-
-// Test / driver code (temporary)
-console.log("tweet data:", $tweet); // to see what it looks like
-$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-
-
-
+  //
+  renderTweets(data);
 
 
 });
